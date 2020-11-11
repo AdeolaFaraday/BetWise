421914cf0e7e1c4b43ee15c061780ec919f302fa
Adeola Apanisile <adexconly@gmail.com>
2020-11-10 17:05:22 -0700

    reverse process

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -6,11 +6,11 @@ const Layout = ({children}) => {
     <div>
     <header>
       <nav>
-      <Link to="/"><button className="btn btn-outline-warning responsive inline-b">Previous Result</button></Link>
+      <Link to="/"><button className="btn btn-outline-warning responsive inline-b">H2H Result</button></Link>
         <h3 class="nav-title"><i class="far fa-futbol"></i>
           BetWise
         </h3>
-        <Link to="/h2h"><button className="btn btn-outline-warning responsive inline-b">H2H Result</button></Link>
+        <Link to="/previous"><button className="btn btn-outline-warning responsive inline-b">Previous Result</button></Link>
       </nav>
     </header>
 

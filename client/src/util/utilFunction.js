2625730e273361e8e80823d2e8de7d2584a39505
AdeolaFaraday <adexconly@gmail.com>
2021-02-17 04:59:49 -0700

    bug fixes

--- client/src/util/utilFunction.js
+++ client/src/util/utilFunction.js
@@ -1,20 +1,16 @@
 //project is subjected towards future code refactoring, other current external helper function will be moved here.
 
 export const prediction = (arr, teamSelectA) => {
-	let drawArr = arr.filter((a) => a.match_hometeam_score == a.match_awayteam_score);
-	let homeArr = arr.filter((a) => a.match_hometeam_name == teamSelectA);
-	let fArr = arr.filter(
-		(a) => a.match_awayteam_name == teamSelectA && a.match_awayteam_score > a.match_hometeam_score
-	);
-	let sArr = arr.filter(
-		(a) => a.match_hometeam_name == teamSelectA && a.match_hometeam_score > a.match_awayteam_score
-	);
-	let newArr = [...fArr, ...sArr];
-	return `${teamSelectA} has won ${Math.ceil(newArr.length / arr.length) * 100}% i.e ${newArr.length} of the last ${
-		arr.length
-	} matches &
-    ${(sArr.length / homeArr.length) * 100}% i.e ${sArr.length} of their last ${
-		homeArr.length
-	} home matches, also drew ${drawArr.length} of their last ${arr.length} matches
-    `;
-};
+  const toUpper = teamName => teamName.charAt(0).toUpperCase() + teamName.substr(1).toLowerCase()
+
+  const drawRecord = arr.filter((a) => a.match_hometeam_score == a.match_awayteam_score)
+  const homeMatches = arr.filter((a) => a.match_hometeam_name == toUpper(teamSelectA))
+  const homeRecordWin = arr.filter((a) => a.match_awayteam_name == toUpper(teamSelectA) && a.match_awayteam_score > a.match_hometeam_score)
+  const awayRecordWin = arr.filter((a) => a.match_hometeam_name == toUpper(teamSelectA) && a.match_hometeam_score > a.match_awayteam_score)
+  const newArr = [...homeRecordWin, ...awayRecordWin]
+  const percentTotalWin = (newArr.length / arr.length).toFixed(2) * 100
+  const percentHomeWin = awayRecordWin.length / homeMatches.length * 100
+  return `${teamSelectA} has won ${percentTotalWin}% i.e ${newArr.length} of the last ${arr.length} matches &
+  ${percentHomeWin}% i.e ${awayRecordWin.length} of their last ${homeMatches.length} home matches, also drew ${drawRecord.length} of their last ${arr.length} matches
+  `
+}
